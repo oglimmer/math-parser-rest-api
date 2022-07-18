ba9ab7ce0118242e2dfc3890d3d77ad2277d6492
Oli Zimpasser <oglimmer@gmail.com>
2022-07-19 01:38:10 +0200

    Add field infos for OpenAPI doc

--- src/dto/DTOs.hpp
+++ src/dto/DTOs.hpp
@@ -10,7 +10,15 @@ class TokenElement : public oatpp::DTO {
 
     DTO_INIT(TokenElement, DTO)
 
+    DTO_FIELD_INFO(type) {
+        info->description = "Token type";
+    }
+
     DTO_FIELD(String, type);
+
+    DTO_FIELD_INFO(data) {
+        info->description = "Token data";
+    }
 
     DTO_FIELD(String, data);
 
@@ -20,6 +28,10 @@ class TokenizeResponse : public oatpp::DTO {
 
     DTO_INIT(TokenizeResponse, DTO)
 
+    DTO_FIELD_INFO(tokens) {
+        info->description = "Tokenized input expression";
+    }
+
     DTO_FIELD(List < Object < TokenElement >>, tokens);
 
 };
@@ -27,6 +39,10 @@ class TokenizeResponse : public oatpp::DTO {
 class ASTResponse : public oatpp::DTO {
 
     DTO_INIT(ASTResponse, DTO)
+
+    DTO_FIELD_INFO(rootExpression) {
+        info->description = "Abstract Syntax Tree for the parsed input expression";
+    }
 
     DTO_FIELD(Fields < Any >, rootExpression);
 
@@ -37,13 +53,33 @@ class CalcResponse : public oatpp::DTO {
 
     DTO_INIT(CalcResponse, DTO)
 
+    DTO_FIELD_INFO(result) {
+        info->description = "The result of the input expression";
+    }
+
     DTO_FIELD(Float64, result);
+
+    DTO_FIELD_INFO(parsedExpression) {
+        info->description = "An internal representation of the parsed input expression";
+    }
 
     DTO_FIELD(String, parsedExpression);
 
-    DTO_FIELD(Object<ASTResponse>, ast);
+    DTO_FIELD_INFO(ast) {
+        info->description = "Abstract Syntax Tree for the parsed input expression";
+    }
+
+    DTO_FIELD(Object < ASTResponse >, ast);
+
+    DTO_FIELD_INFO(tokens) {
+        info->description = "Tokenized input expression";
+    }
 
     DTO_FIELD(List < Object < TokenElement >>, tokens);
+
+    DTO_FIELD_INFO(processingTimeInMicros) {
+        info->description = "Internal processing time in microseconds";
+    }
 
     DTO_FIELD(Int32, processingTimeInMicros);
 
